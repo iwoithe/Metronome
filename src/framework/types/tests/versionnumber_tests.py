090490bab2d5d1d57b2ba3ed7413bf55b63c36b5
I Woithe <iwoithe@just42.net>
2023-07-27 20:37:59 +0930

    :lipstick: Fix code style in versionnumber_tests.py

--- src/framework/types/tests/versionnumber_tests.py
+++ src/framework/types/tests/versionnumber_tests.py
@@ -1,26 +1,32 @@
 from framework.types import VersionNumber, VersionNumberSuffix
 
-def testVersionNumberMajor() -> bool:
+
+def testVersionNumberMajor():
     versionNumber: VersionNumber = VersionNumber(1, 0, 0)
     assert versionNumber.major == 1
 
-def testVersionNumberMinor() -> bool:
+
+def testVersionNumberMinor():
     versionNumber: VersionNumber = VersionNumber(0, 3, 0)
     assert versionNumber.minor == 3
 
-def testVersionNumberMicro() -> bool:
+
+def testVersionNumberMicro():
     versionNumber: VersionNumber = VersionNumber(0, 0, 2)
     assert versionNumber.micro == 2
 
-def testVersionNumberSuffix() -> bool:
+
+def testVersionNumberSuffix():
     versionNumber: VersionNumber = VersionNumber(4, 1, 0, VersionNumberSuffix.DEVELOPMENT)
     assert versionNumber.suffix == VersionNumberSuffix.DEVELOPMENT
 
-def testVersionNumberSuffixNum() -> bool:
+
+def testVersionNumberSuffixNum():
     versionNumber: VersionNumber = VersionNumber(3, 2, 0, VersionNumberSuffix.BETA, 2)
     assert versionNumber.suffixNum == 2
 
-def testVersionNumber__str__() -> bool:
+
+def testVersionNumber__str__():
     versionNumber: VersionNumber = VersionNumber(4, 1, 0, VersionNumberSuffix.ALPHA, 2)
     assert str(versionNumber) == "4.1.0-alpha.2"
 
