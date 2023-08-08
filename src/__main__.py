c5715750b460313a95d1156f3c3b6b4e673bc432
I Woithe <iwoithe@just42.net>
2023-08-08 12:53:39 +0930

    :sparkles: Use different audio backend on Windows

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,10 @@
+import os
 import sys
 
 from app import App
+
+if sys.platform == "win32":
+    os.environ['QT_MULTIMEDIA_PREFERRED_PLUGINS'] = 'windowsmediafoundation'
 
 if __name__ == "__main__":
     app = App()
